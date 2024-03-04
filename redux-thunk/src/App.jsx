74dca2be1ebc8fc3bb5c5e30203b0977c0fc3520
AbhinavRajeshKalvi <abhinav.rajesh@kalvium.community>
2024-03-04 12:09:35 +0530

    0

--- redux-thunk/src/App.jsx
+++ redux-thunk/src/App.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
-import { FetchData } from './utils/redux/action'
-import { useDispatch, useSelector } from 'react-redux'
+import { React, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { FetchData } from "./utils/action.js";
 
-const App = () => {
-
-  const dispatch = useDispatch()
-
-  const users = useSelector(storeData => storeData.user)
-  console.log(users)
-
+function App() {
+  const dispatch = useDispatch();
+  const users = useSelector((storeData) => storeData.user);
   return (
-    <div>
-      <button onClick={() => dispatch(FetchData())}>GET DATA</button>
-      {users.map((e)=>{
-        return <fieldset key={e.id}>
-          <legend>{e.username}</legend>
-          <h1>{e.name}</h1>
-          <h1>{e.email}</h1>
-        </fieldset>
+    <>
+      <button
+        onClick={() => {
+          dispatch(FetchData());
+        }}
+      >
+        GET DATA
+      </button>
+      {users.map((e, i) => {
+        return (
+          <fieldset key={i}>
+            <legend>{e.username}</legend>
+            <h1>{e.name}</h1>
+            <h1>{e.email}</h1>
+          </fieldset>
+        );
       })}
-    </div>
+    </>
   );
-};
+}
 
 export default App;
--- redux-thunk/src/main.jsx
+++ redux-thunk/src/main.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { fetchStore } from './utils/redux/store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { fetchStore } from "./utils/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={fetchStore}>
     <App />
   </Provider>
-  </>
-)
+);
